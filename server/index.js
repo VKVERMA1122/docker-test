7332b1ac1c0a2500d2c1c12dcdc2496246f1f44a
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-03-07 15:24:04 +0530

    hello world

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.get("/", (req, res) => {
-	res.status(200).json("Hello 11");
+	res.status(200).json("Hello World");
 });
 
 app.listen(PORT, () => {
